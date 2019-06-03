4fba30efd6f333f271451e2463472f06fc6062f6
陈韡韬 <cwt194536@alibaba-inc.com>
2019-06-03 14:10:00 +0800

    Updates

---
+++ precache-manifest.e32285f48bb235a83c3a457410664360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6fba1ec2523b268a29554f23957ff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "050f761c89610309b473",
+    "url": "/static/css/2.7ff19518.chunk.css"
+  },
+  {
+    "revision": "25d5ce604c4a4170b3cf",
+    "url": "/static/css/main.1e411466.chunk.css"
+  },
+  {
+    "revision": "050f761c89610309b473",
+    "url": "/static/js/2.2530d6b1.chunk.js"
+  },
+  {
+    "revision": "25d5ce604c4a4170b3cf",
+    "url": "/static/js/main.1a5732c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
